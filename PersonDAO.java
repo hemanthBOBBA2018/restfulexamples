8380e552d4561e929237239ac389792e90d40868
hemanthBOBBA2018 <43779919+hemanthBOBBA2018@users.noreply.github.com>
2019-01-16 19:16:57 +0530

    Add files via upload

---
+++ PersonDAO.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+ 
+@Repository
+public interface PersonDAO extends CrudRepository<Person, Long> {
+ 
+    public List<Person> findByFullNameLike(String name);
+ 
+    public List<Person> findByDateOfBirthGreaterThan(String date);
+ 
+}
---
+++ WelcomeController.java
@@ -0,0 +1,83 @@
+package com.example.demo;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class WelcomeController {
+	private static Logger logger = Logger.getLogger(WelcomeController.class);
+
+	@Autowired
+	private PersonDAO personDAO;
+
+	@SuppressWarnings("unchecked")
+	@RequestMapping("/")
+	public ModelAndView welcome(Map<String, Object> model) {
+			ModelAndView mav = new ModelAndView("MyWelcome1");
+		mav.addObject("message", "I am from H2 database");
+		Iterable<Person> all = personDAO.findAll();
+		List<Person> persons = new ArrayList<Person>();
+		for(Person person : all){
+			persons.add(person);
+		}
+		logger.info("list of person = " + persons);
+		JSONObject jsonObj = new JSONObject();
+		jsonObj.put("persons", persons);
+		mav.addObject("data", jsonObj);
+		return mav;
+	}
+
+	@ResponseBody
+	@RequestMapping("/getName")
+	public String index() {
+		Iterable<Person> all = personDAO.findAll();
+
+		StringBuilder sb = new StringBuilder();
+
+		all.forEach(p -> sb.append(p.getFullName() + "<br>"));
+		System.out.println(sb.toString());
+		return sb.toString();
+	}
+	@RequestMapping("/create")
+	public String crete(Map<String, Object> map) {
+		Person person = new Person();
+		map.put("person", person);
+		return "person";
+	}
+	@SuppressWarnings("unchecked")
+	@ResponseBody
+	@RequestMapping(value="/add", method=RequestMethod.POST)
+	public ModelAndView create(@ModelAttribute Person person, BindingResult result, Map<String, Object> map) throws ParseException {
+		System.out.println(person.getFullName()+"nfjkgnhbjkfdgbjk");
+		System.out.println(person.getDateOfBirth()+"nfjkgnhbjkfdgbjk");
+	/*	Date d1 = df.parse(person.getDateOfBirth().toString());
+		person.setDateOfBirth(d1);*/
+		personDAO.save(person);
+		ModelAndView mav = new ModelAndView("welcome");
+		mav.addObject("message", "I am from H2 database");
+		Iterable<Person> all = personDAO.findAll();
+		List<Person> persons = new ArrayList<Person>();
+		for(Person person1 : all){
+			persons.add(person1);
+		}
+		logger.info("list of person = " + persons);
+		JSONObject jsonObj = new JSONObject();
+		jsonObj.put("persons", persons);
+		mav.addObject("data", jsonObj);
+		return mav;
+	}
+}
